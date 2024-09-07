be24eeefe7c2fc5c1fd92650e1e929e79d30dada
Kristaps Līdacis <lidacisk@gmail.com>
2024-09-07 11:47:28 +0300

    Add Book/Review migrations and factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
+use App\Models\Review;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,33 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Book::factory(33)->create()->each(function ($book){
+            $goodCount = random_int(5, 15);
+            $badCount = random_int(5, 15);
+            $averageCount = random_int(5, 15);
+
+            Review::factory()
+                ->count($goodCount)
+                ->good()
+                ->for($book)
+                ->create();            
+            
+            Review::factory()
+                ->count($badCount)
+                ->bad()
+                ->for($book)
+                ->create();             
+            
+            Review::factory()
+                ->count($averageCount)
+                ->average()
+                ->for($book)
+                ->create(); 
+        });
     }
 }
