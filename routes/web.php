1001f9082302141dda4951170ebf9f6ac834921f
Kristaps Līdacis <lidacisk@gmail.com>
2024-09-08 11:10:54 +0300

    Add Book resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    $books = Book::highestRated()->limit(10)->get();
+    dd($books);
+
     return view('welcome');
 });
+
+Route::resource('books', BookController::class);
